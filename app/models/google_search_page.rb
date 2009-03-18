6b267f8553f847f94a40e3c980a95dd936fdf57e
Mert Torun <mert@merttorun.com>
2009-03-18 03:21:20 +0200

    changed namespace from google-search to gsearch

--- app/models/google_search_page.rb
+++ app/models/google_search_page.rb
@@ -1,49 +1,59 @@
 class GoogleSearchPage < Page
+  attr_reader :query, :start, :gsearch
+  
   description 'A page to display search results obtained through the Google API'
   
   desc 'Renders its contents when there is no search executed yet'
-  tag 'google-search:clean' do |tag|
-    tag.expand if @query.blank?
+  tag 'gsearch:clean' do |tag|
+    tag.expand if query.blank?
   end
   
   desc 'Renders the error message when there is one'
-  tag 'google-search:error' do |tag|
-    unless @gsearch.response_status == 200
-      "#{@gsearch.response_status}: #{@gsearch.response_details}"
+  tag 'gsearch:error' do |tag|
+    unless gsearch.response_status == 200
+      "#{gsearch.response_status}: #{gsearch.response_details}"
     end
   end
   
   desc 'Renders its contents when there are no results'
-  tag 'google-search:no-results' do |tag|
-    tag.expand if @gsearch.response_data.cursor.estimated_result_count.to_i.zero?
+  tag 'gsearch:no-results' do |tag|
+    tag.expand unless query and gsearch.response_data.results.count.zero?
   end
   
   desc 'Renders its contents when there are results'
-  tag 'google-search:results' do |tag|
-    tag.expand if @gsearch.response_data.cursor.estimated_result_count.to_i > 0
+  tag 'gsearch:results' do |tag|
+    tag.expand if gsearch and gsearch.response_data.results.count > 0
   end
   
   desc 'Sets the context for each result in the set'
-  tag 'google-search:results:each' do |tag|
-    @gsearch.response_data.results.each do |result|
+  tag 'gsearch:results:each' do |tag|
+    output = ""
+    gsearch.response_data.results.each do |result|
       tag.locals.result = result
-      output << tag.expand
+      output << result.title + ' - '
     end
     output
   end
   
   [:title_no_formatting, :content, :visible_url, :url, :title, :gsearch_result_class, :unescaped_url,:cache_url].each do |sym|
     desc "Renders the #{sym.to_s.humanize} of the current result"
-    tag "google-search:results:each:#{sym.to_s}" do |tag|
+    tag "gsearch:results:each:#{sym.to_s}" do |tag|
       tag.locals.result.send sym
     end
   end
   
   def process(request,response)
-    @query = request.params['q']
-    @start = request.params['start'] || '0'
-    @gsearch = GoogleSearch.new @query, @start
+    if @query = request.parameters['q']
+      @start = request.parameters['start'] || '0'
+      @gsearch = GoogleSearch.new @query, @start
+      puts "Found #{@gsearch.response_data.results.count} results for #{@query}"
+    end
     super
+    puts 'Processing complete'
+  end
+  
+  def cache?
+    false
   end
   
 end
--- app/models/google_search_tags.rb
+++ app/models/google_search_tags.rb
@@ -2,7 +2,7 @@ module GoogleSearchTags
   include Radiant::Taggable
   
   desc 'The namespace for all Google search tags'
-  tag 'google-search' do |tag|
+  tag 'gsearch' do |tag|
     tag.expand
   end
   
