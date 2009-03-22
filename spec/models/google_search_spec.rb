c2764cf9bf3a81f7af1a808acb714e735286e511
Mert Torun <mert@merttorun.com>
2009-03-23 01:54:00 +0200

    first trials at writing specs

--- spec/models/google_search_spec.rb
+++ spec/models/google_search_spec.rb
@@ -1,11 +1,55 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe GoogleSearch do
-  before(:each) do
-    @google_search = GoogleSearch.new
+  describe "with the query 'google'" do
+    subject do
+      GoogleSearch.new('google').response_data
+    end
+    
+    it do 
+      should have(8).results
+    end
+    it do
+      subject.results.first.should respond_to(:title, :url)
+    end
   end
-
-  it "should be valid" do
-    @google_search.should be_valid
+  
+  describe "with the query 'hsdnjklhopewrngbepvdshrb'" do
+    subject do
+      GoogleSearch.new('hsdnjklhopewrngbepvdshrb').response_data
+    end
+    
+    it do
+      should have(0).results
+    end
+  end
+  
+  describe "with dummy JSON respopnse" do
+    subject do
+      dummy_json = %|
+        { 
+          'TextKey'  : 'text',  
+          'ArrayKey' : [
+            'text in array',
+            { 'TextKey'  : 'text in hash in array' },
+            [ 'text in array in array', 'text in array in array' ]
+          ],
+          'HashKey' : {
+            'TextKey' : 'text in hash',
+            'ArrayKey': [ 'text in array in hash', 'text in array in hash' ],
+            { 'TextKey'  : 'text in hash in hash' }
+          }
+        }
+      |
+      
+      ActiveResource::HttpMock.respond_to do |mock|
+        mock.get '/ajax/services/search/web', {}, dummy_json, 200
+      end
+      GoogleSearch.new('whatever')
+    end
+    
+    it do
+      should respond_to :text_key, :array_key, :hash_key
+    end 
   end
 end
