e1d3950e3c3ba918bf60482e55c64fb86523d1bc
Mert Torun <mert@merttorun.com>
2009-03-25 02:33:28 +0200

    modified gsearch:form tag to pass the contents into the text box

--- app/models/google_search_tags.rb
+++ app/models/google_search_tags.rb
@@ -6,14 +6,14 @@ module GoogleSearchTags
     tag.expand
   end
   
-  desc 'Renders a search form. Targets the first Google Search page in the database by default or the given @url@ attribute. Use the @submit@ attribute to specify the text on  the submit button. All other attributes are passed to the form tag.'
+  desc 'Renders a search form. Targets the first Google Search page in the database by default or the given @url@ attribute. Use the @submit@ attribute to specify the text on the submit button. All other attributes are passed to the form tag. The contents of the tag are entered in the search box.'
   tag 'gsearch:form' do |tag|
     form_url = tag.attr.delete('url') || GoogleSearchPage.first.url
     submit_label = tag.attr.delete('submit') || 'Search' 
     form_attrs = tag.attr.to_a.map{|a| "#{a[0]}=\"#{a[1]}\"" }.join(' ')
     %Q{
       <form action="#{form_url}" method="get" #{form_attrs}>
-        <input type="text" name="q" />
+        <input type="text" name="q" value="#{tag.expand}"/>
         <input type="submit" value="#{submit_label}" />
       </form>
     }
